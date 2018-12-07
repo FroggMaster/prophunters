50e35c3cfd3743b36e2fa1c8db883dc2b21e47ee
Tom <tomy199517@gmail.com>
2018-12-07 22:03:29 +0100

    autotaunt 30s

--- gamemode/cl_autotaunt.lua
+++ gamemode/cl_autotaunt.lua
@@ -1,6 +1,6 @@
 --Taunt automatically every 30s
 local frequency = 2
-local time = 60/(frequency * 2)
+local time = 60/(frequency)
 timer.Create( "autotaunt", time, 0, function()
 	if LocalPlayer():Team() == 3 then
         RunConsoleCommand("ph_taunt_random") 
