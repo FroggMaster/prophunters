e8b006e167663394665b01d21a37714163789842
Tom <tomy199517@gmail.com>
2018-12-07 21:03:31 +0100

    random alone hunter

--- gamemode/sv_teams.lua
+++ gamemode/sv_teams.lua
@@ -1,9 +1,7 @@
-
-
 function GM:TeamsSetupPlayer(ply)
 	local cops = team.NumPlayers(2)
 	local robbers = team.NumPlayers(3)
-	if robbers <= cops then
+	if cops >= 1 then
 		ply:SetTeam(3)
 	else
 		ply:SetTeam(2)
@@ -14,7 +12,7 @@ concommand.Add("car_jointeam", function (ply, com, args)
 	local curteam = ply:Team()
 	local newteam = tonumber(args[1] or "") or 0
 	if newteam == 1 && curteam != 1 then
-
+		
 		ply:SetTeam(newteam)
 		if ply:Alive() then
 			ply:Kill()
@@ -24,37 +22,23 @@ concommand.Add("car_jointeam", function (ply, com, args)
 		ct:Add(" changed team to ")
 		ct:Add(team.GetName(newteam), team.GetColor(newteam))
 		ct:SendAll()
-
+		
 	elseif newteam >= 2 && newteam <= 3 && newteam != curteam then
-
-		// make sure we can't join the bigger team
-		local otherteam = newteam == 2 and 3 or 2
-		if team.NumPlayers(newteam) <= team.NumPlayers(otherteam) then
-			ply:SetTeam(newteam)
-			if ply:Alive() then
-				ply:Kill()
-			end
-			local ct = ChatText()
-			ct:Add(ply:Nick())
-			ct:Add(" changed team to ")
-			ct:Add(team.GetName(newteam), team.GetColor(newteam))
-			ct:SendAll()
-		else
-			local ct = ChatText()
-			ct:Add("Team full, you cannot join")
-			ct:Send(ply)
-		end
-
+		
+		local ct = ChatText()
+		ct:Add("Team full, you cannot join")
+		ct:Send(ply)
+		
 	end
-
+	
 end)
 
 function GM:CheckTeamBalance()
 	if !self.TeamBalanceCheck || self.TeamBalanceCheck < CurTime() then
 		self.TeamBalanceCheck = CurTime() + 3 * 60 // check every 3 minutes
-
-		local diff = team.NumPlayers(2) - team.NumPlayers(3)
-		if diff < -1 || diff > 1 then // teams must be off by more than 2 for team balance
+		
+		local hunter = team.NumPlayers(2)
+		if hunter > 1 then // Il doit y avoir un seul hunter
 			self.TeamBalanceTimer = CurTime() + 30 // balance in 30 seconds
 			for k,ply in pairs(player.GetAll()) do
 				ply:ChatPrint("Auto team balance in 30 seconds")
@@ -68,38 +52,32 @@ function GM:CheckTeamBalance()
 end
 
 function GM:BalanceTeams(nokill)
-	local diff = team.NumPlayers(2) - team.NumPlayers(3)
-	if diff < -1 || diff > 1 then // teams must be off by more than 2 for team balance
-		local biggerTeam, smallerTeam = 3,2
-		if diff > 0 then
-			biggerTeam = 2
-			smallerTeam = 3
+	local hunter = team.NumPlayers(2)
+	while hunter > 1 do
+		local players = team.GetPlayers(2)
+		local ply = players[math.random(#players)]
+		ply:SetTeam(3)
+		if !nokill && ply:Alive() then
+			ply:Kill()
 		end
-		diff = team.NumPlayers(biggerTeam) - team.NumPlayers(smallerTeam)
-		while diff > 1 do
-			local players = team.GetPlayers(biggerTeam)
-			local ply = players[math.random(#players)]
-			ply:SetTeam(smallerTeam)
-			if !nokill && ply:Alive() then
-				ply:Kill()
-			end
-			local ct = ChatText()
-			ct:Add(ply:Nick())
-			ct:Add(" team balanced to ")
-			ct:Add(team.GetName(smallerTeam), team.GetColor(smallerTeam))
-			ct:SendAll()
-			diff = diff - 2
-		end
+		local ct = ChatText()
+		ct:Add(ply:Nick())
+		ct:Add(" est devenu un ")
+		ct:Add(team.GetName(smallerTeam), team.GetColor(smallerTeam))
+		ct:SendAll()
+		hunter = hunter - 1
 	end
 end
 
 function GM:SwapTeams()
 	for k, ply in pairs(player.GetAll()) do
-		if ply:Team() == 2 then
-			ply:SetTeam(3)
-		elseif ply:Team() == 3 then
-			ply:SetTeam(2)
-		end
+		ply:SetTeam(3)
+	end
+	local players = team.GetPlayers(3)
+	local ply = players[math.random(#players)]
+	ply:SetTeam(2)
+	if !nokill && ply:Alive() then
+		ply:Kill()
 	end
 	local ct = ChatText()
 	ct:Add("Teams have been swapped", Color(50, 220, 150))
